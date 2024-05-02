b4d4cc336434d62ae1d5813e4b3932f72ff7ab8e
LucasCrespo75 <lucascrespo_rodrigues@live.com>
2024-05-02 00:10:40 -0300

    feat add: estilo login e componentes do figma

---
+++ src/app/models/usuario.ts
@@ -0,0 +1,8 @@
+export class Usuario {
+    username: string | undefined;
+    nome: string | undefined;
+    senha: string | undefined;
+    genero: string | undefined;
+    telefone: string | undefined;
+
+}
