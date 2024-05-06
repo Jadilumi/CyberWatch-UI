7b9497d486fe80c95957fcd454ee06fd220c23c4
LucasCrespo75 <lucascrespo_rodrigues@live.com>
2024-05-05 22:20:35 -0300

    feat add: app

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import axios from 'axios';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  private readonly URL = environment.apiUrl;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  userData = {
+    username: '',
+    senha: ''
+  };
+
+  logar() {
+    axios.post(`${this.URL}/auth/login`, this.userData).then(
+      response => {
+        console.log("Logou po", response.data)
+      }).catch(error => {
+      console.error(error);
+    })
+  }
+}
