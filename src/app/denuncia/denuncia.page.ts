40eb176ec661ebdfe11d437b7d5264c422c6cb7e
Jailton Júnior DEV <jailtonjr99@hotmail.com.br>
2024-05-27 21:52:25 -0300

    agora os dados do json estão sendo capturados ao clicar do botão

--- src/app/denuncia/denuncia.page.ts
+++ src/app/denuncia/denuncia.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-denuncia',
@@ -7,8 +7,38 @@ import { Component } from '@angular/core';
 })
 export class DenunciaPage {
 
-  
-  constructor() { }
+
+  constructor() {
+  }
+
+  alertButtons = [
+    {
+      text: 'Não',
+      cssClass: 'alert-button-cancel',
+    },
+    {
+      text: 'Sim',
+      cssClass: 'alert-button-confirm',
+      handler: () => {
+        this.salvarDenuncia();
+      }
+    },
+  ];
 
 
+  denunciaData = {
+    "genero": "",
+    "estado": "",
+    "tipoDeBullying": "",
+    "relator": "",
+    "localDoOcorrido": "",
+    "telefone": "",
+    "email": "",
+    "nomeDaVitima": "",
+    "relato": ""
+  }
+
+  salvarDenuncia() {
+    console.log(this.denunciaData)
+  }
 }
